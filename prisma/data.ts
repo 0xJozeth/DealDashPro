3fc9818fc4582a95f31f914597e10cd36088e98f
Glen <0xJozeth@gmail.com>
2023-11-02 19:48:08 -0400

    schema.prisma updates, Navigator

--- prisma/data.ts
+++ prisma/data.ts
@@ -1,188 +1,82 @@
-import { PrismaClient, EnumStatus } from "@prisma/client";
-import { url } from 'inspector';
+import { PrismaClient, EnumStatus, EnumFavoriteCategory } from "@prisma/client";
+import { url } from "inspector";
 import { v4 as uuidv4 } from "uuid";
 
-export type OfferData = {
-  image: string;
-  address: string;
-  dateSubmitted: Date;
-  offerSubmitted: string;
-  status: "Accepted" | "Pending" | "Denied";
-  cancel: boolean;
-  userId: string;
-};
+// export type OfferData = {
+//   image: string;
+//   address: string;
+//   dateSubmitted: Date;
+//   offerSubmitted: string;
+//   status: "Accepted" | "Pending" | "Denied";
+//   cancel: boolean;
+//   userId: string;
+// };
 
-export const OfferData: OfferData[] = [
-  {
-    image: "/prop1.png",
-    address: "645 Parkwood St, Jacksonville, FL 32207",
-    dateSubmitted: new Date(),
-    offerSubmitted: "$224,000",
-    status: EnumStatus.Pending,
-    cancel: false,
-    userId: "kp_2bffb8fd600a4643bc0acda106ab0a0e"
-  },
-  {
-    image: "/prop2.png",
-    address: "4078 Spring Park Cir, Jacksonville, FL 32207",
-    dateSubmitted: new Date(),
-    offerSubmitted: "156,000",
-    status: EnumStatus.Accepted,
-    cancel: false,
-    userId: "kp_2bffb8fd600a4643bc0acda106ab0a0e"
+// export const OfferData: OfferData[] = [
+//   {
+//     image: "/prop1.png",
+//     address: "645 Parkwood St, Jacksonville, FL 32207",
+//     dateSubmitted: new Date(),
+//     offerSubmitted: "$224,000",
+//     status: EnumStatus.Pending,
+//     cancel: false,
+//     userId: "kp_2bffb8fd600a4643bc0acda106ab0a0e",
+//   },
+//   {
+//     image: "/prop2.png",
+//     address: "4078 Spring Park Cir, Jacksonville, FL 32207",
+//     dateSubmitted: new Date(),
+//     offerSubmitted: "156,000",
+//     status: EnumStatus.Accepted,
+//     cancel: false,
+//     userId: "kp_2bffb8fd600a4643bc0acda106ab0a0e",
+//   },
+//   {
+//     image: "/prop3.png",
+//     address: "4078 Spring Park Cir, Jacksonville, FL 32207",
+//     dateSubmitted: new Date(),
+//     offerSubmitted: "$70,000",
+//     status: EnumStatus.Denied,
+//     cancel: false,
+//     userId: "kp_2bffb8fd600a4643bc0acda106ab0a0e",
+//   },
+//   {
+//     image: "/prop4.png",
+//     address: "645 Parkwood St, Jacksonville, FL 32207",
+//     dateSubmitted: new Date(),
+//     offerSubmitted: "$224,000",
+//     status: EnumStatus.Pending,
+//     cancel: false,
+//     userId: "kp_2bffb8fd600a",
+//   },
+//   {
+//     image: "/prop5.png",
+//     address: "4078 Spring Park Cir, Jacksonville, FL 32207",
+//     dateSubmitted: new Date(),
+//     offerSubmitted: "$156,000",
+//     status: EnumStatus.Accepted,
+//     cancel: false,
+//     userId: "kp_2bffb8fd600a",
+//   },
+//   {
+//     image: "/prop6.png",
+//     address: "4078 Spring Park Cir, Jacksonville, FL 32207",
+//     dateSubmitted: new Date(),
+//     offerSubmitted: "$70,000",
+//     status: EnumStatus.Denied,
+//     cancel: false,
+//     userId: "kp_2bffb8fd600a",
+//   },
+// ];
 
-  },
+export const FavoritesData = [
   {
-    image: "/prop3.png",
-    address: "4078 Spring Park Cir, Jacksonville, FL 32207",
-    dateSubmitted: new Date(),
-    offerSubmitted: "$70,000",
-    status: EnumStatus.Denied,
-    cancel: false,
-    userId: "kp_2bffb8fd600a4643bc0acda106ab0a0e"
-
-  },
-  {
-    image: "/prop4.png",
-    address: "645 Parkwood St, Jacksonville, FL 32207",
-    dateSubmitted: new Date(),
-    offerSubmitted: "$224,000",
-    status: EnumStatus.Pending,
-    cancel: false,
-    userId: "kp_2bffb8fd600a"
-  },
-  {
-    image: "/prop5.png",
-    address: "4078 Spring Park Cir, Jacksonville, FL 32207",
-    dateSubmitted: new Date(),
-    offerSubmitted: "156,000",
-    status: EnumStatus.Accepted,
-    cancel: false,
-    userId: "kp_2bffb8fd600a"
-
-  },
-  {
-    image: "/prop6.png",
-    address: "4078 Spring Park Cir, Jacksonville, FL 32207",
-    dateSubmitted: new Date(),
-    offerSubmitted: "$70,000",
-    status: EnumStatus.Denied,
-    cancel: false,
-    userId: "kp_2bffb8fd600a"
-
+    id: 1,
+    userId: "1",
+    propertyId: 1,
+    propertyType: "Single Family",
+    favoriteTags: "Spacious, Modern",
+    favoriteCategory: "NeedsRehab",
+    lastVisitedDate: new Date(),
   },
 ];
-
-export type FavoritesDataProps = {
-  url: string;
-    imgSrc: string;   
-    imgWidth: number;
-    imgHeight: number;
-    alt: string;      
-    cn: string;  
-    askPrice: string;
-    arv     : string;
-    location: string;
-    beds    : number;
-    baths   :number;
-    sqft    : number;
-    userId: string | null
-}
-
-export const FavoritesData: FavoritesDataProps[] = [
-  {
-    url: uuidv4(),
-    imgSrc: "/demo1.png",   
-    imgWidth: 246,
-    imgHeight: 131,
-    alt: "demo",      
-    cn: "aspect-square object-cover",  
-    askPrice: "$224,000",
-    arv     : "$350,000",
-    location: "Jacksonville, FL 32207",
-    beds    : 4,
-    baths   :2,
-    sqft    : 1894,
-    userId: "kp_2bffb8fd600a"
-  },
-  {
-    url: uuidv4(),
-    imgSrc: "/prop1.png",   
-    imgWidth: 246,
-    imgHeight: 131,
-    alt: "demo",      
-    cn: "aspect-square object-cover",  
-
-    askPrice: "$224,000",
-    arv     : "$350,000",
-    location: "Jacksonville, FL 32207",
-    beds    : 4,
-    baths   :2,
-    sqft    : 1894,
-    userId: "kp_2bffb8fd600a"
-  },
-  {
-    url: uuidv4(),
-    imgSrc: "/prop1.png",   
-    imgWidth: 246,
-    imgHeight: 131,
-    alt: "demo",      
-    cn: "aspect-square object-cover",  
-
-    askPrice: "$224,000",
-    arv     : "$350,000",
-    location: "Jacksonville, FL 32207",
-    beds    : 4,
-    baths   :2,
-    sqft    : 1894,
-    userId: "kp_2bffb8fd600a"
-  },
-  {
-    url: uuidv4(),
-    imgSrc: "/prop2.png",   
-    imgWidth: 246,
-    imgHeight: 131,
-    alt: "demo",      
-    cn: "aspect-square object-cover",  
-
-    askPrice: "$224,000",
-    arv     : "$350,000",
-    location: "Jacksonville, FL 32207",
-    beds    : 4,
-    baths   :2,
-    sqft    : 1894,
-    userId: "kp_2bffb8fd600a4643bc0acda106ab0a0e"
-  },
-  {
-    url: uuidv4(),
-    imgSrc: "/prop3.png",   
-    imgWidth: 246,
-    imgHeight: 131,
-    alt: "demo",      
-    cn: "aspect-square object-cover",  
-
-    askPrice: "$224,000",
-    arv     : "$350,000",
-    location: "Jacksonville, FL 32207",
-    beds    : 4,
-    baths   :2,
-    sqft    : 1894,
-    userId: "kp_2bffb8fd600a4643bc0acda106ab0a0e"
-  },
-  {
-    url: uuidv4(),
-    imgSrc: "/prop4.png",   
-    imgWidth: 246,
-    imgHeight: 131,
-    alt: "demo",      
-    cn: "aspect-square object-cover",  
-
-    askPrice: "$224,000",
-    arv     : "$350,000",
-    location: "Jacksonville, FL 32207",
-    beds    : 4,
-    baths   :2,
-    sqft    : 1894,
-    userId: "kp_2bffb8fd600a4643bc0acda106ab0a0e"
-  },
-]
--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,315 +1,318 @@
-import { faHeart, faHeartCirclePlus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import { faHeart, faHeartCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const propImages = [
-	{
-		name: 'prop1',
-		props: {
-			src: '/prop1.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop2',
-		props: {
-			src: '/prop2.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop3',
-		props: {
-			src: '/prop3.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop4',
-		props: {
-			src: '/prop4.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop5',
-		props: {
-			src: '/prop5.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop6',
-		props: {
-			src: '/prop6.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop1',
-		props: {
-			src: '/prop1.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop2',
-		props: {
-			src: '/prop2.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop3',
-		props: {
-			src: '/prop3.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop4',
-		props: {
-			src: '/prop4.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop5',
-		props: {
-			src: '/prop5.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	{
-		name: 'prop6',
-		props: {
-			src: '/prop6.png',
-			id: 12913781,
-			width: 350,
-			height: 115,
-			alt: 'property',
-			className: 'object-cover aspect-square max-h-[115px]',
-			arv: '$325,000',
-			price: '$273,000',
-			location: 'Duval County, Jacksonville, FL',
-			beds: '3',
-			baths: '2',
-			sqft: '1800',
-		},
-	},
-	// Add more items to the propImages array if needed
+  {
+    name: "prop1",
+    props: {
+      src: "/prop1.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop2",
+    props: {
+      src: "/prop2.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop3",
+    props: {
+      src: "/prop3.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop4",
+    props: {
+      src: "/prop4.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop5",
+    props: {
+      src: "/prop5.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop6",
+    props: {
+      src: "/prop6.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop1",
+    props: {
+      src: "/prop1.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop2",
+    props: {
+      src: "/prop2.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop3",
+    props: {
+      src: "/prop3.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop4",
+    props: {
+      src: "/prop4.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop5",
+    props: {
+      src: "/prop5.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  {
+    name: "prop6",
+    props: {
+      src: "/prop6.png",
+      id: 12913781,
+      width: 350,
+      height: 115,
+      alt: "property",
+      className: "object-cover aspect-square max-h-[115px]",
+      arv: "$325,000",
+      price: "$273,000",
+      location: "Duval County, Jacksonville, FL",
+      beds: "3",
+      baths: "2",
+      sqft: "1800",
+    },
+  },
+  // Add more items to the propImages array if needed
 ];
 
 function Navigator() {
-	return (
-		<div className='fixed top-[15%] right-16 flex justify-center items-center z-[9999]'>
-			<div className='m-8 p-8 w-[600px] h-[800px] min-w-[300px] rounded-[10px] bg-slate-50 z-[100] shadow-xl'>
-				<form className='flex justify-between items-center w-full gap-x-2 p-2 pl-4 mb-4 rounded-full border neutral-500 bg-white'>
-					<input
-						type='text'
-						placeholder='Search by City or State'
-						className='w-1/2  border-r border-neutral-200 text-neutral-500 text-base-font-normal leading-normal focus:outline-none'
-					/>
-					<input
-						type='number'
-						placeholder='Price'
-						className='text-base-font-normal leading-normal focus:outline-none'
-					/>
-					<button type='submit' title='Search'>
-						<Image
-							src='/search1.svg'
-							width={32}
-							height={32}
-							alt='search'
-							className='flex justify-center items-center'
-						/>
-					</button>
-				</form>
-				<div className='grid grid-cols-2 gap-4 overflow-y-scroll max-h-[90%] scrollbar-hide !scroll-smooth'>
-					{propImages.map((item, index) => (
-						<div
-							key={index}
-							className='relative rounded-t-[10px] overflow-hidden'
-						>
-							{' '}
-							{/* TODO: Add  */}
-							<FontAwesomeIcon
-								icon={faHeart}
-								className='absolute top-4 right-4 text-white cursor-pointer shadow-lg'
-							/>
-							<Link href={`/property-details/${item.props.id}`}>
-								<Image
-									src={item.props.src}
-									width={item.props.width}
-									height={item.props.height}
-									alt={item.props.alt}
-									className={item.props.className}
-								/>
-								<div className='flex flex-col p-4 border border-zinc-300 rounded-b-[10px] gap-2'>
-									<div className='flex justify-between items-center'>
-										<p className='text-zinc-950 text-sm font-normal leading-[14px]'>
-											{item.props.price}
-										</p>
-										<div className='flex justify-between items-center gap-[2px]'>
-											<p className='text-neutral-500 text-xs font-normal leading-[14px]'>
-												ARV:{' '}
-											</p>
-											<p className='text-neutral-500 text-xs font-normal leading-[14px]'>
-												{item.props.arv}
-											</p>
-										</div>
-									</div>
-									<div className='flex justify-start items-center'>
-										<p className='text-zinc-950 text-sm font-normal leading-[14px] whitespace-nowrap'>
-											{item.props.location}
-										</p>
-									</div>
-									<div className='flex justify-start items-center gap-2'>
-										<div className='flex justify-between items-center gap-[2px]'>
-											<p className='text-neutral-500 text-xs font-normal leading-[14px]'>
-												{item.props.beds}
-											</p>
-											<p className='text-neutral-500 text-xs font-normal leading-[14px]'>
-												bds
-											</p>
-										</div>
-										<div className='flex justify-between items-center gap-[2px]'>
-											<p className='text-neutral-500 text-xs font-normal leading-[14px]'>
-												{item.props.baths}
-											</p>
-											<p className='text-neutral-500 text-xs font-normal leading-[14px]'>
-												ba
-											</p>
-										</div>
-										<div className='flex justify-between items-center gap-[2px]'>
-											<p className='text-neutral-500 text-xs font-normal leading-[14px]'>
-												{item.props.sqft}
-											</p>
-											<p className='text-neutral-500 text-xs font-normal leading-[14px]'>
-												sqft
-											</p>
-										</div>
-									</div>
-								</div>
-							</Link>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="fixed right-16 top-[15%] z-[9999] flex items-center justify-center">
+      <div className="z-[100] m-8 h-[800px] w-[600px] min-w-[300px] rounded-[10px] bg-slate-50 p-8 shadow-xl">
+        <form
+          id="searchBar"
+          className="neutral-500 mb-4 flex w-full items-center justify-between gap-x-2 rounded-full border bg-white p-2 pl-4"
+        >
+          <input
+            type="text"
+            placeholder="Search by City or State"
+            className="text-base-font-normal  w-1/2 border-r border-neutral-200 leading-normal text-neutral-500 focus:outline-none"
+          />
+          <input
+            type="number"
+            placeholder="Price"
+            className="text-base-font-normal leading-normal focus:outline-none"
+          />
+          <button type="submit" title="Search">
+            <Image
+              src="/search1.svg"
+              width={32}
+              height={32}
+              alt="search"
+              className="flex items-center justify-center"
+            />
+          </button>
+        </form>
+        <div className="scrollbar-hide grid max-h-[90%] grid-cols-2 gap-4 overflow-y-scroll !scroll-smooth">
+          {propImages.map((item, index) => (
+            <div
+              key={index}
+              className="relative overflow-hidden rounded-t-[10px]"
+            >
+              {" "}
+              {/* TODO: Add  */}
+              <FontAwesomeIcon
+                icon={faHeart}
+                className="absolute right-4 top-4 cursor-pointer text-white shadow-lg"
+              />
+              <Link href={`/property-details/${item.props.id}`}>
+                <Image
+                  src={item.props.src}
+                  width={item.props.width}
+                  height={item.props.height}
+                  alt={item.props.alt}
+                  className={item.props.className}
+                />
+                <div className="flex flex-col gap-2 rounded-b-[10px] border border-zinc-300 p-4">
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm font-normal leading-[14px] text-zinc-950">
+                      {item.props.price}
+                    </p>
+                    <div className="flex items-center justify-between gap-[2px]">
+                      <p className="text-xs font-normal leading-[14px] text-neutral-500">
+                        ARV:{" "}
+                      </p>
+                      <p className="text-xs font-normal leading-[14px] text-neutral-500">
+                        {item.props.arv}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-start">
+                    <p className="whitespace-nowrap text-sm font-normal leading-[14px] text-zinc-950">
+                      {item.props.location}
+                    </p>
+                  </div>
+                  <div className="flex items-center justify-start gap-2">
+                    <div className="flex items-center justify-between gap-[2px]">
+                      <p className="text-xs font-normal leading-[14px] text-neutral-500">
+                        {item.props.beds}
+                      </p>
+                      <p className="text-xs font-normal leading-[14px] text-neutral-500">
+                        bds
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between gap-[2px]">
+                      <p className="text-xs font-normal leading-[14px] text-neutral-500">
+                        {item.props.baths}
+                      </p>
+                      <p className="text-xs font-normal leading-[14px] text-neutral-500">
+                        ba
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between gap-[2px]">
+                      <p className="text-xs font-normal leading-[14px] text-neutral-500">
+                        {item.props.sqft}
+                      </p>
+                      <p className="text-xs font-normal leading-[14px] text-neutral-500">
+                        sqft
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navigator;

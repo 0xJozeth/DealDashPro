82bb95df59eae4f8034f936508b2be436c550c19
Glen <0xJozeth@gmail.com>
2023-11-10 01:04:45 -0500

    rendering server data on the property details page with react-query

--- src/app/property-details/[propertyId]/page.tsx
+++ src/app/property-details/[propertyId]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import MoreProperties from "@/components/Property Details/MoreProperties";
 import NearbyProperties from "@/components/Property Details/NearbyProperties";
@@ -5,26 +7,43 @@ import PropertyImages from "@/components/Property Details/PropertyImages";
 import PropertyMain from "@/components/Property Details/PropertyMain";
 import PropertyTitle from "@/components/Property Details/PropertyTitle";
 import { db } from "@/db";
+import { Property } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import Image from "next/image";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { use } from "react";
 
-const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
-  const property = await db.property.findFirst({
-    where: {
-      id: params.id,
+const PropertyDetailsPage = () => {
+  const params = useParams();
+
+  console.log("params", params);
+
+  const {
+    data: property,
+    refetch,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["property"],
+    queryFn: async () => {
+      const { data } = await axios.get(`/api/property/${params.propertyId}`);
+      return data as Property;
     },
   });
 
-  console.log("params", params);
-  console.log("property", property);
-
+  if (!property) {
+    return <div className="min-h-screen">Loading...</div>;
+  }
   return (
     <>
       <MaxWidthWrapper>
-        <PropertyTitle data={property} />
-        <PropertyImages data={property} />
-        <PropertyMain data={property} />
-        <MoreProperties data={property} />
-        <NearbyProperties data={property} />
+        <PropertyTitle property={property} />
+        <PropertyImages property={property} />
+        <PropertyMain />
+        <MoreProperties />
+        <NearbyProperties />
       </MaxWidthWrapper>
     </>
   );
--- src/components/Property Details/PropertyTitle.tsx
+++ src/components/Property Details/PropertyTitle.tsx
@@ -1,19 +1,21 @@
 import { Property } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import Link from "next/link";
 
-const PropertyTitle = ({ data }: { data: Property }) => {
+const PropertyTitle = ({ property }: { property: Property }) => {
   return (
     <div className="flex h-auto w-full flex-col py-2">
       <div className="flex h-[1.8rem] items-center py-4">
         <h1 className="text-[26px] font-medium leading-[30px] text-black">
-          {data.heading}
+          {property.heading}
         </h1>
       </div>
 
       <div className="flex h-[.625rem] items-center justify-between py-4">
         <div className="flex h-[.625rem] items-center gap-16">
           <p className="text-sm font-normal leading-tight text-black">
-            {data.county}, {data.city}, {data.state}
+            {property.county}, {property.city}, {property.state}
           </p>
           <Link
             href="#"
@@ -24,7 +26,8 @@ const PropertyTitle = ({ data }: { data: Property }) => {
         </div>
         <div className="flex h-[.625rem] items-center justify-between">
           <p className="text-sm font-normal leading-[14px] text-neutral-500">
-            Deal Posted: {data.createdAt.toDateString()}
+            Deal Posted:{" "}
+            {new Date(property.createdAt).toLocaleDateString("en-US")}
           </p>
         </div>
       </div>
